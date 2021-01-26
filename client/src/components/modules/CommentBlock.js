25570016f872e34acdc69c3bb0b788f689433c62
Alexis Yang <alexis@Alexiss-MacBook-Air.local>
2021-01-25 16:26:48 -0800

    fixed time difference

--- client/src/components/modules/CommentBlock.js
+++ client/src/components/modules/CommentBlock.js
@@ -34,6 +34,7 @@ class CommentsBlock extends Component {
         comments: [],
         commentId: null,
         commentsDisplay : [],
+        currentProgress: null,
         timers : [],
         isPaused : null,
         winWidth : window.innerWidth,
@@ -50,7 +51,7 @@ class CommentsBlock extends Component {
         userId: comment.spotifyId,
         top: this.getRandomNumber(60, 600), 
         left: this.getRandomNumber(100, this.state.winWidth-100)}])
-      if (comment.spotifyId === this.props.userId) {
+      if (comment.spotifyId !== this.props.userId) {
         console.log('getting song progress')
         get("/api/currentState").then((state) => {
           console.log('progress ' + state.body.progress_ms)
